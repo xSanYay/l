04a32a1f5bd793b96d6f4e8e2afa1aed4d09aab7
xSanYay <145270952+xSanYay@users.noreply.github.com>
2024-10-08 13:35:32 +0530

    Update d.m

--- d.m
+++ d.m
@@ -1,26 +1,21 @@
 M = 2; numBits = 1e3; chipRate = 10; snr = 10; fs = 1000;
 
-% Generate random bits and perform BPSK modulation
 dataBits = randi([0, 1], numBits, 1);
 modulatedData = 2*dataBits - 1;
 
-% Generate and modulate PN sequence
+
 pnSequence = 2*randi([0, 1], numBits*chipRate, 1) - 1;
 spreadSignal = repelem(modulatedData, chipRate) .* pnSequence;
-
-% Transmit over AWGN channel and despread
 receivedSignal = awgn(spreadSignal, snr, 'measured');
 despreadSignal = receivedSignal .* pnSequence;
 despreadBits = sum(reshape(despreadSignal, chipRate, numBits), 1)' / chipRate;
 
-% Demodulation and BER calculation
 receivedBits = despreadBits > 0;
 BER_DSSS = mean(dataBits ~= receivedBits);
 
-% Spectrum calculation using a common function
 spectrumPlot = @(x) fftshift(fft(x, length(x)));
 
-% Plotting
+
 figure;
 titles = {'Message Signal', 'PN Code', 'Spread Signal', 'Received Signal', 'Despread Signal', 'Demodulated Signal'};
 data = {modulatedData, pnSequence, spreadSignal, receivedSignal, despreadSignal, despreadBits};
@@ -31,5 +26,11 @@ for i = 1:6
     xlabel('Frequency (Hz)'); ylabel('Magnitude');
     grid on;
 end
-
 fprintf('DSSS BER: %e\n', BER_DSSS);
+t = (0:length(data{i})-1) / fs; 
+    % Plot the signal in time domain
+    plot(t, data{i});
+    title([titles{i}, ' (Time Domain)']);
+    xlabel('Time (s)');
+    ylabel('Amplitude');
+    grid on;
