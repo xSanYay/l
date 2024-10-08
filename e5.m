102020498d06b0d96f774b274b13d5f6b667b575
xSanYay <145270952+xSanYay@users.noreply.github.com>
2024-10-08 15:32:17 +0530

    Update e5.m

--- e5.m
+++ e5.m
@@ -9,7 +9,7 @@ for k = 1:length(SNRdB)
     data = randi([0 M-1], numSymbols, 1); 
     modSignal = qammod(data, M, 'UnitAveragePower', true); 
     I = real(modSignal); Q = imag(modSignal);
-    rx = (1+impairments(1))I + i(1-impairments(1))*Q; 
+    rx = (1+impairments(1))I *i(1-impairments(1))*Q; 
     rx = rx .* exp(1i*impairments(2)) + impairments(3) + 1i*impairments(4); 
     rx = awgn(rx, SNRdB(k), 'measured'); 
     SER(k) = sum(data ~= qamdemod(rx, M, 'UnitAveragePower', true)) / numSymbols; 
