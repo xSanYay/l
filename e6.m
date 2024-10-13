cc12481d78877d13b5857b7c476c09fc69c65f9f
xSanYay <145270952+xSanYay@users.noreply.github.com>
2024-10-13 18:08:52 +0530

    Update e6.m

--- e6.m
+++ e6.m
@@ -1,94 +1,88 @@
-M_values = [2, 4, 8, 16, 32];
-Eb_N0_dB = 0:20;
-Eb_N0 = 10.^(Eb_N0_dB / 10);
-Ps_mpsk = zeros(length(M_values), length(Eb_N0));
+EbN0_db = 0:2:24;
+EbN0 = 10.^(EbN0_db/10);
 
+%MPSK
+M_PSK = [2, 4, 8, 16, 32];
+P_SER_PSK = zeros(length(M_PSK), length(EbN0));
 
-for i = 1:length(M_values)
-    M = M_values(i);
-    
-    Pe_mpsk = qfunc(sqrt(2 * Eb_N0 * sin(pi/M)^2));
-
-    Ps_mpsk(i, :) = 1 - (1 - Pe_mpsk).^log2(M);
+for i = 1:length(M_PSK)
+M = M_PSK(i);
+P_SER_PSK(i,:) = 2 * qfunc(sqrt(2*EbN0*sin(pi/M)));
 end
 
-% Plotting
-figure;
-semilogy(Eb_N0_dB, Ps_mpsk','-o');
-grid on;
-xlabel('Eb/N0 (dB)');
-ylabel('Probability of Symbol Error (Ps)');
-title('Probability of Symbol Error for MPSK');
-legend('M=2', 'M=4', 'M=8', 'M=16', 'M=32');
-M_values = [2, 4, 8, 16, 32];
-Eb_N0_dB = 0:20;
-Eb_N0 = 10.^(Eb_N0_dB / 10);
-Ps_mpam = zeros(length(M_values), length(Eb_N0));
-for i = 1:length(M_values)
-    M = M_values(i);  
-    Pe_mpam = qfunc(sqrt(3 * log2(M) * Eb_N0 / (M^2 - 1)));
-    Ps_mpam(i, :) = 1 - (1 - Pe_mpam).^log2(M);
+%MPAM
+M_PAM = [2, 4, 8, 16];
+P_SER_PAM = zeros(length(M_PAM), length(EbN0));
+
+for i = 1:length(M_PAM)
+M = M_PAM(i);
+P_SER_PAM(i,:) = 2 *(M-1)/M * qfunc(sqrt(6*log2(M)/(M^2 - 1) * EbN0));
 end
 
-% Plotting
-figure;
-semilogy(Eb_N0_dB, Ps_mpam','-o');
-grid on;
-xlabel('Eb/N0 (dB)');
-ylabel('Probability of Symbol Error (Ps)');
-title('Probability of Symbol Error for MPAM');
-legend('M=2', 'M=4', 'M=8', 'M=16', 'M=32');
-M_values = [2, 4, 8, 16, 32];
-Eb_N0_dB = 0:20;
-Eb_N0 = 10.^(Eb_N0_dB / 10);
-Ps_mqam = zeros(length(M_values), length(Eb_N0));
-
-for i = 1:length(M_values)
-    M = M_values(i);
-    
-    Pe_mqam = 2 * (1 - 1/sqrt(M)) * qfunc(sqrt(3 * log2(M) * Eb_N0 / (2 * (M - 1))));
-    
-    Ps_mqam(i, :) = 1 - (1 - Pe_mqam).^log2(M);
+%MQAM
+M_QAM = [4,16,64,256];
+P_SER_QAM = zeros(length(M_QAM), length(EbN0));
+for i = 1:length(M_QAM)
+M = M_QAM(i);
+P_SER_QAM(i,:) = 4 * (1 - 1/sqrt(M)) * qfunc(sqrt(3*log2(M)/(M-1) * EbN0));
 end
 
-
-figure;
-semilogy(Eb_N0_dB, Ps_mqam','-o');
-grid on;
-xlabel('Eb/N0 (dB)');
-ylabel('Probability of Symbol Error (Ps)');
-title('Probability of Symbol Error for MQAM');
-legend('M=2', 'M=4', 'M=8', 'M=16', 'M=32');
-M_values = [2, 4, 8, 16, 32];
-Eb_N0_dB = 0:20;
-Eb_N0 = 10.^(Eb_N0_dB / 10);
-Ps_mfsk = zeros(length(M_values), length(Eb_N0));
-for i = 1:length(M_values)
-    M = M_values(i);
-    Ps_mfsk(i, :) = 2 * (1 - qfunc(sqrt((6 * log2(M)) / (M^2 - 1) * Eb_N0)));
-end
-% Plotting
-figure;
-semilogy(Eb_N0_dB, Ps_mfsk','-o');
-grid on;
-xlabel('Eb/N0 (dB)');
-ylabel('Probability of Symbol Error (Ps)');
-title('Probability of Symbol Error for MFSK');
-legend('M=2', 'M=4', 'M=8', 'M=16', 'M=32');
-M_values = [2, 4, 8, 16, 32];
-Eb_N0_dB = 0:20;
-Eb_N0 = 10.^(Eb_N0_dB / 10);
-Ps_mfsk_noncoherent = zeros(length(M_values), length(Eb_N0));
-for i = 1:length(M_values)
-    M = M_values(i);    
-    Ps_mfsk_noncoherent(i, :) = 2 * (1 - qfunc(sqrt((3 * log2(M)) / (2 * (M^2 - 1)) * Eb_N0)));
+%MFSK (coherent)
+M_MFSK = [2, 4, 8, 16, 32]; % M values for MFSK
+P_SER_MFSK_coherent = zeros(length(M_MFSK), length(EbN0));
+for i = 1:length(M_MFSK)
+M = M_MFSK(i);
+P_SER_MFSK_coherent(i, :) = qfunc(sqrt(2*EbN0*log2(M)/(M-1)));
 end
 
-% Plotting
-figure;
-semilogy(Eb_N0_dB, Ps_mfsk_noncoherent','-o');
+%MFSK (non coherent)
+P_SER_MFSK_noncoherent = zeros(length(M_MFSK), length(EbN0));
+for i = 1:length(M_MFSK)
+M = M_MFSK(i);
+P_SER_MFSK_noncoherent(i, :) = exp(-(EbN0/2)) .* (((1+EbN0).^(M-1) - 1)./(M-1));
+end
+
+subplot(2,3,1)
+for i=1:length(M_PSK)
+semilogy(EbN0_db,P_SER_PSK(i,:),'-o'); hold on;
+end
+title('MPSK');
+xlabel('Eb/N0 in db')
+ylabel('SER');
 grid on;
-xlabel('Eb/N0 (dB)');
-ylabel('Probability of Symbol Error (Ps)');
-title('Probability of Symbol Error for Non-coherent MFSK');
-legend('M=2', 'M=4', 'M=8', 'M=16', 'M=32');
+
+subplot(2,3,2)
+for i=1:length(M_PAM)
+semilogy(EbN0_db,P_SER_PAM(i,:),'-o'); hold on;
+end
+title('MPAM');
+xlabel('Eb/N0 in db')
+ylabel('SER');
+grid on;
+
+subplot(2,3,3)
+for i=1:length(M_QAM)
+semilogy(EbN0_db,P_SER_QAM(i,:),'-o'); hold on;
+end
+title('MQAM');
+xlabel('Eb/N0 in db')
+ylabel('SER');
+grid on;
+
+subplot(2,3,4)
+for i=1:length(M_MFSK)
+semilogy(EbN0_db,P_SER_MFSK_coherent(i,:),'-o'); hold on;
+end
+title('MFSK coherent');
+xlabel('Eb/N0 in db')
+ylabel('SER');
+grid on;
+
+subplot(2,3,5)
+for i=1:length(M_MFSK)
+semilogy(EbN0_db,P_SER_MFSK_noncoherent(i,:),'-o'); hold on;
+end
+title('MFSK noncoherent');
+xlabel('Eb/N0 in db')
+ylabel('SER');
+grid on;
